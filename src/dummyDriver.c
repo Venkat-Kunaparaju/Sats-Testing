d924040f4e8511b5f0d10b1cba313489b3433553
VenkatKunaparaju <venkatkrox@gmail.com>
2023-02-27 15:41:36 -0500

    Read driver

--- src/dummyDriver.c
+++ src/dummyDriver.c
@@ -1,2 +1,14 @@
 /* Code to emulate (not really) a driver */
 
+#include "../include/PSP.h"
+
+/* 
+Drivers would read from line, storing into the buffer specified by the user, 
+and the user would then be able to acess the contents in the buffer
+*/
+int read(int fd, void * buf) {
+    char * temp = "This is what was read in";
+    memcpy(buf, temp, strlen(temp));
+
+    return 1;
+}
