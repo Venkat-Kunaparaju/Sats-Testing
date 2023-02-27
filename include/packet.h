51386bc06765db6f6fe16e151982aa8f9479d64e
VenkatKunaparaju <venkatkrox@gmail.com>
2023-02-27 16:11:32 -0500

    Read

--- include/packet.h
+++ include/packet.h
@@ -2,6 +2,8 @@
 
 struct buffer * packetRead_BUSNAME_(void);
 
+int packetWrite_BUSNAME_(struct buffer *);
+
 struct segment{
     unsigned char priSourcePort;
     unsigned char destPortFlags;
--- src/packet.c
+++ src/packet.c
@@ -2,6 +2,10 @@
 
 #include "../include/PSP.h"
 
+
+/*
+Read packets from bus, and forward to transport layer
+*/
 struct buffer * packetRead_BUSNAME_() {
     /* 
     Size based on what stage the sat is currently in. It would adapt to what size is expected from the sender.
@@ -9,9 +13,20 @@ struct buffer * packetRead_BUSNAME_() {
     stage, and this can be accomplished with a simple if statement in this function.
     */
     struct buffer * buf = requestBuffer(30); 
-    read(0, (void *)buf->data);
+    read(0, (void *)buf->data); //Blocks on bus
 
     /* Assignment of priority and initalization of other header information done here */
 
+    /* Forwarding to transport code done here */
+
     return buf;
+}
+
+
+/*
+Write packets to bus, that came from transport layer (due to error or application sending data)
+*/
+int packet packetWrite_BUSNAME_(struct buffer * buf) {
+    write()
+
 }
